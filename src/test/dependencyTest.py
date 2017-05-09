29e919b9ea27fa1b841951a9ad8acbf25bfa6a9c
filiprak <filiprak@wp.pl>
2017-05-09 17:08:45 +0200

    Refactor tests

--- src/test/dependencyTest.py
+++ src/test/dependencyTest.py
@@ -7,6 +7,7 @@ import os
 parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 os.sys.path.insert(0,parentdir) 
 from semantic_analyzer.semantic import SemanticAnalyzer
+from errors import LogicError
 from jsonparser.jsontypes import JSONObject, JSONPair, JSONArray, JSONString
 
 
@@ -24,21 +25,24 @@ a.append(JSONPair("imports", imp))
 b = JSONObject()
 b.append(JSONPair("moduleName", JSONString("B")))
 imp = JSONArray()
-imp.insert(JSONString("A"))
+imp.insert(JSONString("C"))
 b.append(JSONPair("imports", imp))
 
 c = JSONObject()
 c.append(JSONPair("moduleName", JSONString("C")))
 imp = JSONArray()
-c.append(JSONPair("imports", imp))
+#imp.insert(JSONString("A"))
+#c.append(JSONPair("imports", imp))
 
 testcase["a.json"] = a
 testcase["b.json"] = b
 testcase["c.json"] = c
 
-analyzer.parsedDict = testcase
-print analyzer.dependencySort(testcase)
-
+try:
+    analyzer.parsedDict = testcase
+    print analyzer.dependencySort(testcase)
+except LogicError as e:
+    print e.message
 
 
 
